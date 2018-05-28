5cca001b2ca4562de3fa620ddfe40baa81b814b1
Patrick Qu <quteng.bo@gmail.com>
2018-05-28 11:46:30 -0400

    customers

--- app.js
+++ app.js
@@ -1,16 +1,48 @@
-document.querySelector('.button').addEventListener('click', loadData);
+// document.querySelector('.button').addEventListener('click', loadData);
 
-function loadData() {
-  // Create an XHR Object
+// function loadData() {
+//   // Create an XHR Object
+//   const xhr = new XMLHttpRequest();
+
+//   // Open
+//   xhr.open('Get', 'data.txt', true);
+
+//   xhr.onload = function() {
+//     if(this.status === 200) {
+//       console.log(this.responseText);
+//     }
+//   }
+//   xhr.send();
+// }
+
+document.querySelector('#button2').addEventListener('click', loadCustomers);
+
+function loadCustomers() {
+  
   const xhr = new XMLHttpRequest();
 
-  // Open
-  xhr.open('Get', 'data.txt', true);
+  xhr.open('GET', 'customers.json', true);
 
+  let output = '';
   xhr.onload = function() {
     if(this.status === 200) {
-      console.log(this.responseText);
-    }
+      console.log(JSON.parse(this.responseText));
+      const customers = JSON.parse(this.responseText);
+      customers.forEach(function(customer) {
+        output += `
+        <ul>
+          <li>ID: ${customer.id}</li>
+          <li>Name: ${customer.name}</li>
+          <li>Company: ${customer.company}</li>
+          <li>Phone: ${customer.phone}</li>
+        </ul>
+        `
+      });
+
+      document.getElementById('customers').innerHTML = output;
+    } 
   }
+  
+
   xhr.send();
 }
